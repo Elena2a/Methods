229d3d5966e1f0f63f31cec17f5709b3054e52c5
Elena Abaeva <Elena.Abaeva@t-systems.com>
2020-05-30 18:25:49 +0300

    Initial commit

---
+++ src/ru/tsystems/autotest/Main.java
@@ -0,0 +1,13 @@
+package ru.tsystems.autotest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+                Student s = new Student("John", 2);
+                System.out.println("Name is "+ s.getName() +" and roll number is "+ s.getRollNo());
+            }
+        }
+
+
---
+++ src/ru/tsystems/autotest/Student.java
@@ -0,0 +1,21 @@
+package ru.tsystems.autotest;
+
+public class Student {
+        private String name;
+        private int roll_no;
+
+        public Student(String name, int roll_no) {
+            this.name = name;
+            this.roll_no = roll_no;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getRollNo() {
+            return roll_no;
+        }
+    }
+
+
